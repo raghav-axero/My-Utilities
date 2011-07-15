89e0ce1ef70f36e114016cb26c148defb7990613
Raghav Khunger <raghav.khuger@gmail.com>
2011-07-15 17:48:00 +0530

    Methods added

--- scripts/raghav.utilities.js
+++ scripts/raghav.utilities.js
@@ -1,6 +1,6 @@
-﻿if (typeof (RAGHAV.Utilities) == undefined || RAGHAV.Utilities == null) { RAGHAV.Utilities = {}; }
+﻿if (typeof (RAGHAV.Utilities) === undefined || RAGHAV.Utilities === null) { RAGHAV.Utilities = {}; }
 
-if (typeof (RAGHAV.Utilities.UrlHelper) == undefined || RAGHAV.Utilities.UrlHelper == null) { RAGHAV.Utilities.UrlHelper = {}; }
+if (typeof (RAGHAV.Utilities.UrlHelper) === undefined || RAGHAV.Utilities.UrlHelper === null) { RAGHAV.Utilities.UrlHelper = {}; }
 
 
 /*
@@ -14,7 +14,103 @@ if (typeof (RAGHAV.Utilities.UrlHelper) == undefined || RAGHAV.Utilities.UrlHelp
 *	RAGHAV.Utilities.UrlHelper.replaceURLWithHTMLLink("https://github.com") //output: <a href="https://github.com">https://github.com</a>
 */
 RAGHAV.Utilities.UrlHelper = (function() {
+	'use strict';
 	var my = {
+		encode: function(uri) {
+			///	<summary>
+			///	This function encodes special characters. In addition, it encodes the following characters: , / ? : @ & = + $ #
+			///	</summary>
+			///	<param name="url" type="String">The URI to be encoded</param>
+			///	<returns type="String">The encoded URI</returns>
+			return encodeURIComponent(uri);
+		},
+		decode: function(uri) {
+			///	<summary>
+			///	This function decodes special characters. In addition, it decodes the following characters: , / ? : @ & = + $ #
+			///	</summary>
+			///	<param name="url" type="String">The URI to be decoded</param>
+			///	<returns type="String">The decoded URI</returns>
+			return decodeURIComponent(uri);
+		},
+		getCurrentURL: function() {
+			///	<summary>
+			///	Gets the current URL.
+			///	</summary>
+			///	<returns type="String">The current URL</returns>
+			return window.location.href;
+		},
+		getCurrentURLHash: function() {
+			///	<summary>
+			///	Gets that part of current URL that follows the # symbol, including the # symbol.
+			/// Example: '#test'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The hash part of current URL</returns>
+			return window.location.hash;
+		},
+		getCurrentURLHost: function() {
+			///	<summary>
+			///	Gets the host name (without the port number or square brackets)of current URL.
+			/// Example: 'www.google.com'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The host name</returns>
+			return window.location.hostname;
+		},
+		getCurrentURLHostWithPort: function() {
+			///	<summary>
+			///	Gets the host name with port number of current URL.
+			/// Example: '[www.google.com]:80'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The host name</returns>
+			return window.location.host;
+		},
+		getCurrentURLPath: function() {
+			///	<summary>
+			///	Gets the path (relative to the host) of current URL.
+			/// Example: '/search'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The path</returns>
+			return window.location.pathname;
+		},
+		getCurrentURLPathAndQuery: function() {
+			///	<summary>
+			///	Gets the path (relative to the host) and querystring of current URL.
+			/// Example: '/search?q=demo'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The path and querystring of current URL</returns>
+			return window.location.pathname + window.location.search;
+		},
+		getCurrentURLPathQueryAndHash: function() {
+			///	<summary>
+			///	Gets the path (relative to the host) + querystring +hash part of current URL.
+			/// Example: '/search?q=demo#test'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The path and querystring of current URL</returns>
+			return window.location.pathname + window.location.search + window.location.hash;
+		},
+		getCurrentURLPort: function() {
+			///	<summary>
+			///	Gets the current URL port.
+			/// Example: '80'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The port of current URL</returns>
+			return window.location.href;
+		},
+		getCurrentURLProtocol: function() {
+			///	<summary>
+			///	Gets the protocol of current URL.
+			/// Example: 'http:'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The protocol of current URL</returns>
+			return window.location.protocol;
+		},
+		getCurrentURLQueryString: function() {
+			///	<summary>
+			///	Gets the querystring of current URL (i.e. the part of the URL that follows the ? symbol, including the ? symbol.)
+			/// Example: '?q=demo'  from 'http://[www.google.com]:80/search?q=demo#test'
+			///	</summary>
+			///	<returns type="String">The querystring of current URL</returns>
+			return window.location.search;
+		},
 		getQueryStringParamValue: function(parameterName, url) {
 			///	<summary>
 			///		Gets the value of parameter from the querystring.
@@ -27,7 +123,7 @@ RAGHAV.Utilities.UrlHelper = (function() {
 			var regex = new RegExp(regexS);
 			var results = regex.exec(url || window.location.href);
 			if (results == null)
-				return "";
+				return '';
 			else
 				return decodeURIComponent(results[1].replace(/\+/g, " "));
 		},
@@ -43,13 +139,6 @@ RAGHAV.Utilities.UrlHelper = (function() {
 	};
 	return my;
 } ());
-
-
-
-
-
-
-
 
 
 
